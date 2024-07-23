bc3181f7dce005b8ab6fbe6606387af3b539006d
olajumokeolaife <olaifeolajumoke@gmail.com>
2024-07-23 15:27:18 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -22,19 +22,30 @@ import {
   ColorMapping,
   Editor,
 } from "./pages";
+import "./App.css";
 
 import { useStateContext } from "./contexts/ContextProvider";
 
-import "./App.css";
-
 const App = () => {
   const {
+    setCurrentColor,
+    setCurrentMode,
+    currentMode,
     activeMenu,
+    currentColor,
     themeSettings,
     setThemeSettings,
-    currentColor,
-    currentMode,
   } = useStateContext();
+
+  // useEffect(() => {
+  //   const currentThemeColor = localStorage.getItem("colorMode");
+  //   const currentThemeMode = localStorage.getItem("themeMode");
+  //   if (currentThemeColor && currentThemeMode) {
+  //     setCurrentColor(currentThemeColor);
+  //     setCurrentMode(currentThemeMode);
+  //   }
+  // }, []);
+
   return (
     <div className={currentMode === "Dark" ? "dark" : ""}>
       <BrowserRouter>
@@ -43,19 +54,16 @@ const App = () => {
             <TooltipComponent content="Settings" position="Top">
               <button
                 type="button"
-                className="text-3xl p-3
-                 hover:drop-shadow-xl 
-                 hover:bg-light-gray 
-                 text-white"
                 onClick={() => setThemeSettings(true)}
                 style={{ background: currentColor, borderRadius: "50%" }}
+                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
               >
                 <FiSettings />
               </button>
             </TooltipComponent>
           </div>
           {activeMenu ? (
-            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
               <Sidebar />
             </div>
           ) : (
@@ -64,17 +72,15 @@ const App = () => {
             </div>
           )}
           <div
-            className={`dark:bg-main-dark-bg bg-main-bg 
-              min-h-screen w-full
-              ${activeMenu ? "md:ml-72" : "flex-2"}`}
+            className={
+              activeMenu
+                ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  "
+                : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
+            }
           >
-            <div
-              className="fixed md:static bg-main-bg 
-            dark:bg-main-dark-bg navbar w-full"
-            >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
               <Navbar />
             </div>
-
             <div>
               {themeSettings && <ThemeSettings />}
 
@@ -105,6 +111,7 @@ const App = () => {
                 <Route path="/stacked" element={<Stacked />} />
               </Routes>
             </div>
+            <Footer />
           </div>
         </div>
       </BrowserRouter>
